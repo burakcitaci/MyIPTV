a9ea3a23f7daddeacfbde95cbf7ec7aba6b9b8dd
Burak Citaci <burakcitaci@gmail.com>
2020-09-01 20:11:51 +0200

    Projektdateien hinzufügen.

---
+++ MyIPTV/MyIPTV/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using MyIPTV.ViewModels;
+using MyIPTV.Views;
+using Xamarin.Forms;
+
+namespace MyIPTV
+{
+   public partial class AppShell : Xamarin.Forms.Shell
+   {
+      public AppShell()
+      {
+         InitializeComponent();
+         Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+         Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+      }
+
+   }
+}
